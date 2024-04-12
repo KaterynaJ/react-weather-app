c79ad8105de700542b709f8d17727ced5f1846ca
KaterynaJ <114310883+KaterynaJ@users.noreply.github.com>
2024-04-12 10:09:37 -0600

    added bootstrap and deleted old logos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
      Weather App
+
+     <button type="button" className="btn btn-primary">Button test</button>
+     <footer>
+      This project is coded by{" "}<a href="https://www.linkedin.com/in/kateryna-bezpalko-746b0a244/" target='_blanc'>Kate</a>{" "}and is{" "} 
+      <a href="https://github.com/KaterynaJ/shecodes-react-app.git" target="_blanc">open-sourced</a></footer>
     </div>
   );
 }
 
-export default App;
+
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Search from "./Search"
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
         <h1>Weather App</h1>
+        <App/>
         <Search />
-        <a href="https://github.com/KaterynaJ/shecodes-react-app.git" target="_blank" rel="noreferrer">Open-source</a>
+
   </React.StrictMode>
 );
 
