6357fb8999d72e9bb32e11b770be6c0076d30b0e
KaterynaJ <114310883+KaterynaJ@users.noreply.github.com>
2024-05-03 07:38:09 -0600

    time display - minor fix

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -7,12 +7,12 @@ export default function FormattedDate(props) {
     let hours = props.date.getHours();
     let minutes = props.date.getMinutes();
 
-if (minutes < 0) {
-    minutes = '0${minutes}';
+if (minutes < 10) {
+    minutes = `0${minutes}`;
 }
 
-if (hours < 0) {
-    hours = '0${hours}';
+if (hours < 10) {
+    hours = `0${hours}`;
 }
 
     return <div>Last updated: {day} {hours}:{minutes}</div>
