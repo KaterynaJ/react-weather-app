d1c11f86e19a96c6859888b2bd46a5bba7298834
KaterynaJ <114310883+KaterynaJ@users.noreply.github.com>
2024-05-23 12:17:35 -0600

    Centered the app and footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-
-import './App.css';
+import "./App.css";
 import React from "react";
-import Weather from "./Weather.js"
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
-    <div className="App"> 
-
-<Weather defaultCity="Calgary"/>
-     <footer>
-      This project is coded by{" "}<a href="https://www.linkedin.com/in/kateryna-bezpalko-746b0a244/" target='_blanc'>Kate</a>{" "}and is{" "} 
-      <a href="https://github.com/KaterynaJ/shecodes-react-app.git" target="_blanc">open-sourced</a></footer>
+    <div className="App">
+      <Weather defaultCity="Calgary" />
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/kateryna-bezpalko-746b0a244/"
+          target="_blanc">
+          Kate
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/KaterynaJ/shecodes-react-app.git"
+          target="_blanc">
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
-
-
