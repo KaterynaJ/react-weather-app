1628e363ed931431b1caf0742d62d4d15c5476fb
KaterynaJ <114310883+KaterynaJ@users.noreply.github.com>
2024-06-06 16:21:42 -0600

    first attempt to move from CRA to Vite

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import App from "./components/App/App";
 
-
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
@@ -13,4 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
